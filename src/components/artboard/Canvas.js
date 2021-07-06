a1ebe895b227d499badf59b5b1bd6f7554f406b9
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-07-06 21:04:43 +0530

    update: move current shape to top

--- src/components/artboard/Canvas.js
+++ src/components/artboard/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Stage, Layer, Text, Rect } from "react-konva";
 import { useArtboard } from "../../contexts/ArtboardContext";
 import RectangleShape from "../shapes/RectangleShape";
@@ -7,6 +7,8 @@ import TriangleShape from "../shapes/TriangleShape";
 import PentagonShape from "../shapes/PentagonShape";
 
 const Canvas = () => {
+	const canvasRef = useRef();
+
 	const [canvasDimensions, setCanvasDimensions] = useState({ width: 500, height: 500 });
 
 	const { canvasColor, selectedColor, selectedShape, elementsData, setElementsData } = useArtboard();
@@ -15,24 +17,36 @@ const Canvas = () => {
 		setElementsData([...elementsData, { shape: selectedShape, x: e.evt.offsetX, y: e.evt.offsetY, color: selectedColor }]);
 	};
 
+	const handleMouseDown = (item) => {
+		console.log(canvasRef.current);
+		let index = item.target.index;
+		console.log(index);
+		let drawnItems = canvasRef.current.children[0].children;
+		if (drawnItems[index]) {
+			drawnItems[index].moveToTop();
+			console.log("to top");
+		}
+	};
+
 	useEffect(() => {
 		setCanvasDimensions({ width: 0.7 * window.innerWidth, height: window.innerHeight - 5 });
 	}, []);
 
 	return (
-		<Stage width={canvasDimensions.width} height={canvasDimensions.height}>
+		<Stage width={canvasDimensions.width} height={canvasDimensions.height} ref={canvasRef}>
 			<Layer onClick={handleDraw}>
 				<Rect width={canvasDimensions.width} height={canvasDimensions.height} fill={canvasColor} />
 				<Text text={"Click anywhere to draw. Drag elements to move."} fontSize={15} />
 				<Text text={JSON.stringify(elementsData)} fill={selectedColor} y={canvasDimensions.height - 50} />
 				{elementsData.map((element) => {
+					console.log(elementsData);
 					switch (element.shape) {
 						default:
 							return;
 						case "rectangle":
 							return <RectangleShape element={element} />;
 						case "circle":
-							return <CircleShape element={element} />;
+							return <CircleShape element={element} handleMouseDown={handleMouseDown} />;
 						case "triangle":
 							return <TriangleShape element={element} />;
 						case "pentagon":
--- src/components/shapes/CircleShape.js
+++ src/components/shapes/CircleShape.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Circle } from "react-konva";
 
-const CircleShape = ({ element }) => {
-	return <Circle draggable x={element.x} y={element.y} radius={25} fill={element.color} />;
+const CircleShape = ({ element, handleMouseDown }) => {
+	return <Circle draggable x={element.x} y={element.y} radius={25} fill={element.color} onMouseDown={handleMouseDown} />;
 };
 
 export default CircleShape;
